56f2c6160c2d7c621b5b0e09148e99096770cf8f
Isadora Xavier <isinhasena@gmail.com>
2022-04-24 11:44:29 -0300

    config typeorm e migration create/run products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-
+import '@shared/typeorm';
 
 const app = express();
 
@@ -11,21 +12,21 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
-  if (error instanceof AppError){
-    return response.status(error.statusCode).json({
-      status: 'error', 
-      message: error.message
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internaç server error',
     });
-  }
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internaç server error'
-  });
-},
-)
+  },
+);
 
 app.listen(5000, () => {
-  console.log('Server started on port 5000! :rocket: ')
-})
-
+  console.log('Server started on port 5000! :rocket: ');
+});
---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,3 @@
+import { createConnection } from 'typeorm';
+
+createConnection(); // procura pelo código onde está o arquivo que tem as informações para comnexão com o banco
